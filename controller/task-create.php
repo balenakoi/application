187325e9fb4c7a101a6a99104186287b72962a9c
balen <balenakoi@gmail.com>
2018-10-04 09:47:08 +0200

    finishing task files with css

--- controller/task-create.php
+++ controller/task-create.php
@@ -1,1 +1,17 @@
+<?php
+  // File for function
+  include '../model/data.php';
+  if (isset($_POST['send'])) {
+      $name = $_POST['name'];
+      $deadline = $_POST['deadline'];
+      $list_id = $_GET['list_id'];
+      $project_id = $_GET['project_id'];
+      if (isset($list_id) and isset($name) and isset($deadline) and !empty($name) and !empty($list_id) and !empty($deadline)) {
+          insertTask($list_id, $name, $deadline, $checked, $project_id);
+          header("Location: list.php?list_id=" . $list_id . "&project_id=" . $project_id . "");
+      }
+    }
 
+
+  // File for HTML and CSS
+  include '../view/task-createView.php';
--- controller/veriftask.php
+++ controller/veriftask.php
@@ -1,1 +1,16 @@
-
+<?php
+include '../model/db.php';
+if (!empty($_POST['checked'])) {
+  $verif = $bdd->prepare('UPDATE tasks SET checked = 1 WHERE id = :taskid');
+  $verif->execute(array(
+  'taskid' => $_GET['taskid']
+  ));
+  header("location: list.php?list_id=" . $_GET['list_id'] . "&project_id=" . $_GET['project_id'] . "");
+} else {
+  $verif = $bdd->prepare('UPDATE tasks SET checked = 0 WHERE id = :taskid');
+  $verif->execute(array(
+  'taskid' => $_GET['taskid']
+  ));
+  header("location: list.php?list_id=" . $_GET['list_id'] . "&project_id=" . $_GET['project_id'] . "");
+}
+?>
